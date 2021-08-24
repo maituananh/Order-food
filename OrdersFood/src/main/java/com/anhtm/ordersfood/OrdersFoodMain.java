340ce5436d5aa014290d29df5bdfc9e1476b96c5
Tuan Anh Mai <anh.mai@hitachivantara.com>
2021-08-24 17:48:48 +0700

    docker

--- OrdersFood/src/main/java/com/anhtm/ordersfood/OrdersFoodMain.java
+++ OrdersFood/src/main/java/com/anhtm/ordersfood/OrdersFoodMain.java
@@ -2,11 +2,15 @@ package com.anhtm.ordersfood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * The type Orders food main.
  */
 @SpringBootApplication
+@EntityScan(basePackages = {"com.anhtm.ordersfood.entity"})
+@EnableJpaRepositories(basePackages = "com.anhtm.ordersfood.repository")
 public class OrdersFoodMain {
 
     /**
